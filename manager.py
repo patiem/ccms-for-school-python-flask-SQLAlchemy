2ec739add019df9f26cb3baf166022f277be2763
castarox <mika.kamil12@gmail.com>
2017-01-24 14:42:48 +0100

    add init to student, start edit method (student)

---
+++ manager.py
@@ -0,0 +1,20 @@
+from user import *
+
+class Manager(User):
+    
+    def __init__(self, name, last_name, mail, telephone):
+        self.name = name
+        self.last_name = last_name
+        self.main = mail
+        self.telephone = telephone
+
+    def edit_manager(self)
+        pass
+
+    @classmethod
+    def add_manager(cls):
+        pass
+
+    @classmethod
+    def remove_manager(cls):
+        pass
