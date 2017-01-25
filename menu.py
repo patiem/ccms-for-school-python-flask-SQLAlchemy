c700b6b392c998a02fd88f573bf7cd6ae9277e8d
castarox <mika.kamil12@gmail.com>
2017-01-25 15:56:19 +0100

    add show student to menu

--- menu.py
+++ menu.py
@@ -3,8 +3,8 @@ from manager import *
 from student import *
 
 class Menu:
-    logged_user = User.login()
-
+    #logged_user = User.login()
+    pass
 
 class StudentMenu(Menu):
     pass
@@ -22,21 +22,33 @@ class ManagerMenu(Menu):
 
     @staticmethod
     def print_menu(user_object):
-        Ui.clear()
-        Ui.print_head('Logged as {} {}'.format(user_object.name, user_object.last_name, 'header'))
-        Ui.print_head('Manager menu:', 'header')
-        options = '\t1: Add mentor' \
-                  '\t2: Remove Mentor' \
-                  '\t3: Edit Mentor' \
-                  '\t4: Show mentor list' \
-                  '\t5: Show student list'
-        user_choice = Ui.get_menu(options, 1, 5)
+
+        while True:
+            Ui.clear()
+            Ui.print_head('Logged as {} {}'.format(user_object.name, user_object.last_name, 'header'))
+            Ui.print_head('Manager menu:', 'header')
+            options = '\t1: Add mentor\n' \
+                      '\t2: Remove Mentor\n' \
+                      '\t3: Edit Mentor\n' \
+                      '\t4: Show mentor list\n' \
+                      '\t5: Show student list\n'
+            user_choice = Ui.get_menu(options, 1, 5)
+            ManagerMenu.chose_option(user_choice)
 
     @staticmethod
     def chose_option(choice):
         if choice == '1':
-            pass
+            ManagerMenu.add_student()
+        if choice == '2':
+            Ui.print_table(Student.create_list_to_save(Student.pass_list()), ['id', 'name', 'last name', 'mail', 'telephone'])
+        else:
+            exit()
+
     @staticmethod
     def add_student():
-        pass 
+        label_list = ['Name', 'Last Name', 'E-mail', 'telephone']
+        user_data = Ui.get_inputs(label_list)
+        Student.add_user(user_data[0], user_data[1], user_data[2], user_data[3], Student.pass_list())
 
+Student.create_object_list('csv/students.csv', Student.students_list)
+ManagerMenu.print_menu(Student.students_list[0])
