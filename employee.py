aba84115d3d522f2c5f0dea6768ee11cce318ae1
castarox <mika.kamil12@gmail.com>
2017-01-25 13:30:42 +0100

    add pass functions

--- employee.py
+++ employee.py
@@ -4,14 +4,16 @@ from common import *
 class Employee(User):
     employees_list = []
 
-    def __init__(self, id, name, last_name, mail, telephone, account_type='Employee'):
+    def __init__(self, idx, name, last_name, mail, telephone):
         """
         Create Employee object
-        :param id: string (id of student)
+        :param idx: string (id of student)
         :param name: string (name of student)
         :param last_name: string (last name of student)
         :param mail: string  (mail of student)
-        :param type: string (type of account)
         """
-        User.__init__(self, id, name, last_name, mail, telephone)
-        self.account_type = account_type
+        User.__init__(self, idx, name, last_name, mail, telephone)
+
+    @classmethod
+    def pass_list(cls):
+        return cls.employees_list
