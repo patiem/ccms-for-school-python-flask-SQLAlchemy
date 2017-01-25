26d4e2554b20f631065709e9b47d3f2076da4e6d
cj <sigamateusz@gmail.com>
2017-01-25 15:12:04 +0100

    Added run() to Menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,33 @@
 from ui import Ui
+from user import User
 
 
 class Menu:
-    pass
+
+    # @staticmethod
+    # def print_menu():
+    #     raise NotImplementedError()
+
+    @staticmethod
+    def print_menu():
+        raise NotImplementedError()
+
+    @staticmethod
+    def run():
+
+        logged_user = User.login()
+
+        if logged_user[3] == 'student':
+            StudentMenu.print_menu()
+
+        if logged_user[3] == 'mentor':
+            MentorMenu.print_menu()
+
+        if logged_user[3] == 'manager':
+            EmployeeMenu.print_menu()
+
+        if logged_user[3] == 'employee':
+            ManagerMenu.print_menu()
 
 
 class StudentMenu(Menu):
