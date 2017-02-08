0e59fc72d594318408f38449268937ce11c2cb1f
Pati Mikulska <pp.mikulska@gmail.com>
2017-02-08 14:48:12 +0100

    Start, end date in database

--- create_db.sql
+++ create_db.sql
@@ -6,7 +6,7 @@ DROP TABLE `TEAM`;
 DROP TABLE `USERS_CHECKPOINT`;
 DROP TABLE `Users`;
 DROP TABLE `Users_team`;
-CREATE TABLE `Assigments`( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `ID_MENTOR` INTEGER NOT NULL, `TITLE` TEXT NOT NULL, `DATA` TEXT NOT NULL, `LINK` TEXT NOT NULL, `GROUP` TEXT);
+CREATE TABLE `Assigments`( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `ID_MENTOR` INTEGER NOT NULL, `TITLE` TEXT NOT NULL, `START_DATA` TEXT NOT NULL, `END_DATA` TEXT NOT NULL, `LINK` TEXT NOT NULL, `GROUP` TEXT);
 CREATE TABLE `Attendence`( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `ID_STUDENT` INTEGER NOT NULL, `DATE` TEXT NOT NULL, `STATUS` TEXT NOT NULL );
 CREATE TABLE `Chekpoints`( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `ID_STUDENTA` INTEGER NOT NULL, `TITLE` TEXT NOT NULL );
 CREATE TABLE `Sumbissions` ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `ID_ASSIGMENT` TEXT NOT NULL, `ID_STUDENT` INTEGER NOT NULL, `GRADE` INTEGER, `DATE` TEXT NOT NULL, `LINK` TEXT NOT NULL, `ID_MENTOR` INTEGER);
@@ -18,3 +18,4 @@ INSERT INTO Users (ID, Name, Surname, "E-mail", Password, Type) VALUES (1, 'Stud
 INSERT INTO Users (ID, Name, Surname, "E-mail", Password, Type) VALUES (2, 'Mentor', 'Codecool', 'prosty', 'b''\xb2\xf7=\xf0\xc9\xdat\xe1\xa3\xcf\xb7\xe6\x80ee\xff@\r\xf6\x13f\x10|\x18N\xa3\xcbb\x83\xa7r\x85''', 'Mentor');
 INSERT INTO Users (ID, Name, Surname, "E-mail", Password, Type) VALUES (3, 'Employee', 'Codecool', 'nw', 'b''\xb2\xf7=\xf0\xc9\xdat\xe1\xa3\xcf\xb7\xe6\x80ee\xff@\r\xf6\x13f\x10|\x18N\xa3\xcbb\x83\xa7r\x85''', 'Employee');
 INSERT INTO Users (ID, Name, Surname, "E-mail", Password, Type) VALUES (4, 'Manager', 'Codecool', 'jurek', 'b''\xb2\xf7=\xf0\xc9\xdat\xe1\xa3\xcf\xb7\xe6\x80ee\xff@\r\xf6\x13f\x10|\x18N\xa3\xcbb\x83\xa7r\x85''', 'Manager');
+INSERT INTO Users (Name, Surname, "E-mail", Password, Type) VALUES ('Student', 'Codecool', 'ty', 'b''\xb2\xf7=\xf0\xc9\xdat\xe1\xa3\xcf\xb7\xe6\x80ee\xff@\r\xf6\x13f\x10|\x18N\xa3\xcbb\x83\xa7r\x85''', 'Manager');
