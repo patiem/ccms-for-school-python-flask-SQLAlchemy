f803a092150af443b06151848b76088ea85bf1d6
Michał Osak <osakmichal@gmail.com>
2017-03-06 11:40:25 +0100

    main.py route login

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, session
+
 
 app = Flask(__name__)
+app.secret_key = 'any random string'
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    session['username'] = 'jest'
+    if 'username' in session:
+        username = session['username']
+        return 'Logged in as ' + username + '<br>' + \
+               "<b><a href = '/logout'>click here to log out</a></b>"
+
+    #session.pop('username', None)
+    return "You are not logged in <br><a href = '/login'></b>" + \
+       "click here to log in</b></a>"
+
+
 
 
 if __name__ == "__main__":
