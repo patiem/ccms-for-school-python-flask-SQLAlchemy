94014e9d09d2b6ca33388b412c7a8e2e57b4227e
Pati Mikulska <pp.mikulska@gmail.com>
2017-03-06 13:30:51 +0100

    First site

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from models.student import Student
 
 app = Flask(__name__)
 
+logged_student = Student.
 
 @app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('assignments')
+def show_assignments():
+
+@app.route('/assignments/<assignent_id>')
+def show_assignment():
+    logged_user = None
+    return render_template('submissions.html')
+
+
+
+
+
 
 
 if __name__ == "__main__":
