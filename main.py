8cfa1e3780463e1aefcc2fe01565edb0b002cab6
cj <sigamateusz@gmail.com>
2017-03-06 13:20:16 +0100

    Added teams template to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World'
+
+
+@app.route('/teams')
+def teams():
+    return render_template('teams.html')
 
 
 if __name__ == "__main__":
