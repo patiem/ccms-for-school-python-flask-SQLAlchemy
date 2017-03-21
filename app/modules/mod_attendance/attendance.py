0c5e7a12ddd92c2d62e3055bdd0dcfb1953a1ca0
cj <sigamateusz@gmail.com>
2017-03-21 13:41:54 +0100

    Removed unused methods from attendance.py

--- app/modules/mod_attendance/attendance.py
+++ app/modules/mod_attendance/attendance.py
@@ -18,73 +18,6 @@ class Attendance:
         self.date = date
         self.present = present
 
-    # @classmethod
-    # def toggle_present(cls, student, choice):
-    #     """
-    #     Set the presence of a student
-    #     :param student: Attendance object
-    #     :param choice: string (1/2/3)
-    #     :return: None
-    #     """
-    #
-    #     if choice == '1':
-    #         student.present = 'Present'
-    #     elif choice == '2':
-    #         student.present = 'Late'
-    #     elif choice == '3':
-    #         student.present = 'Absent'
-
-
-    # @classmethod
-    # def create_new_day(cls):
-    #     """
-    #     Adds new day to attendance list.
-    #     Creates new rows in attendance.csv for each student
-    #     :return: None
-    #     """
-    #     today = str(datetime.date.today())
-    #
-    #     for student in Student.object_list:
-    #         cls.attendance_list.append(Attendance(student.idx, today, 'Absent'))
-    #
-    #         query = 'INSERT INTO `Attendance`(`ID_STUDENT`,`DATE`,`STATUS`) VALUES (?,?,?)'
-    #         params = list([student.idx, today, 'Absent'])
-    #
-    #         sql.query(query, params)
-
-    # @classmethod
-    # def get_attendance_by_date(cls, date):
-    #     """
-    #     Returns a list of students with date and presence status
-    #     :param date: string (date: DD.MM.YYYY)
-    #     :return: string (list of students with date and presence status)
-    #     """
-    #     output_string = ''
-    #     for attendance in cls.attendance_list:
-    #         if attendance.date == date:
-    #             for student in Student.object_list:
-    #                 if student.id == attendance.id_student:
-    #                     output_string += date + ' ' + student.name + ' ' + student.last_name + ' presence status: ' + \
-    #                                      attendance.present + '\n'
-    #     return output_string[:-1]
-
-    # @classmethod
-    # def get_student_attendance(cls, student_id):
-    #     """
-    #     Returns all students occurrence in attendance_list
-    #     :param student_id: string (user id)
-    #     :return: string (list of all instances)
-    #     """
-    #     output_string = ''
-    #     for attendance in cls.attendance_list:
-    #         if attendance.id_student == student_id:
-    #             for student in Student.object_list:
-    #                 if student.id == attendance.id_student:
-    #                     output_string += attendance.date + ' ' + student.name + ' ' + student.last_name \
-    #                                      + ' presence: ' + attendance.present + '\n'
-    #                     break
-    #     return output_string[:-1]
-
     @classmethod
     def create_attendance_list(cls, date):
         """
@@ -104,24 +37,6 @@ class Attendance:
 
             return attendance_list
         return False
-
-    # @classmethod
-    # def students_engagement(cls):
-    #     """
-    #     Creates list with students attendance.
-    #     Counts the presence of students.
-    #     :return engagement_list: two dimensional list
-    #     """
-    #     engagement_list = []
-    #     for student in Student.object_list:
-    #         student_attendance = {'Present': 0, 'Late': 0, 'Absent': 0}
-    #         for attendance in cls.attendance_list:
-    #             if attendance.id_student == student.idx:
-    #                 student_attendance[attendance.present] += 1
-    #         engagement_list.append([student.name, student.last_name, str(student_attendance['Present']),
-    #                                 str(student_attendance['Late']),
-    #                                 str(student_attendance['Absent'])])
-    #     return engagement_list
 
     @staticmethod
     def get_attendance_list(date):
