a4fdb70c51be535d8c19d3376128efa828734211
Michał Osak <osakmichal@gmail.com>
2017-03-22 22:32:54 +0100

    adding sql_alchemy queries in checkpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
 from app.modules.mod_student.student import Student
 from app.modules.mod_team.team import Team
 from app.modules.mod_user.user import *
