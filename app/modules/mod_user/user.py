0f25a2cf98e674e893c5cfcd8cc137b96c29711d
Kamil <mika.kamil12@gmail.com>
2017-03-23 13:51:17 +0100

    add cascade to relationship

--- app/modules/mod_user/user.py
+++ app/modules/mod_user/user.py
@@ -12,7 +12,7 @@ class User(db.Model):
     Telephone = db.Column(db.Integer)
     Password = db.Column(db.String)
     Type = db.Column(db.String)
-    id_team_object = db.relationship('UsersTeam', backref='student', lazy='joined')
+    id_team_object = db.relationship('UsersTeam', backref='student', cascade='all, delete', lazy='joined')
 
     def __init__(self, idx, name, last_name, mail, telephone, type, password):
         """
@@ -85,7 +85,6 @@ class User(db.Model):
         """
         to_remove = User.query.filter_by(ID=idx).first()
         db.session.delete(to_remove)
-        db.engine.execute(query)
         db.session.commit()
         query = """
                     DELETE FROM Attendance
